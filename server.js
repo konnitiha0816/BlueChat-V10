d9f1f01e1e2fc7d4b3e48cfbe7a371aa20684605
konnitiha0816 <makoto08160122@gmail.com>
2026-02-02 16:30:39 +0900

    Create server.js

---
+++ server.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require("socket.io");
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static('public'));
+const rooms = new Map();
+
+io.on('connection', (socket) => {
+    // 部屋作成
+    socket.on('create-room', (roomId) => {
+        rooms.set(roomId, { hostId: socket.id });
+        socket.join(roomId);
+        socket.emit('room-created', roomId);
+    });
+
+    // 参加リクエスト（ホストのサイドバーへ通知）
+    socket.on('request-join', (data) => {
+        const room = rooms.get(data.roomId);
+        if (room) {
+            io.to(room.hostId).emit('admin-approval-request', { 
+                senderId: socket.id, 
+                nickname: data.nickname 
+            });
+            socket.emit('waiting-approval');
+        } else {
+            socket.emit('join-error', '部屋が見つかりません');
+        }
+    });
+
+    // ホストがサイドバーで承認ボタンを押した時
+    socket.on('approve-user', (targetId) => {
+        io.to(targetId).emit('join-approved');
+    });
+
+    // 通話参加
+    socket.on('join-call', (data) => {
+        socket.join(data.roomId);
+        socket.to(data.roomId).emit('user-connected', {
+            peerId: data.peerId,
+            nickname: data.nickname
+        });
+    });
+
+    // チャット
+    socket.on('send-chat', (data) => {
+        io.to(data.roomId).emit('receive-chat', data);
+    });
+});
+
+server.listen(process.env.PORT || 3000);
